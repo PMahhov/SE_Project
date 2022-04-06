6761296e4b7c259cf4f98ef5f596dbc0860a5b4b
Mathilde Simoni <57211517+mathildesimoni@users.noreply.github.com>
2022-04-06 18:36:26 +0400

    Add timeline_stock class

---
+++ source/timeline_stock.py
@@ -0,0 +1,17 @@
+from source.background_stock import Background_Stock
+
+class Timeline_Stock:
+
+    def __init__(self, id: int, volume: int, avg_price_bought: int, cashflow: int, stock_reference: Background_Stock) -> None:
+        self.id = id
+        self.volume = volume
+        self.price = price
+        self.volatility = volatility
+        self.trend = trend
+        self.historical_prices = historical_prices
+
+    def progress_time() -> None:
+        pass
+
+    def get_price() -> int:
+        return self.price 
