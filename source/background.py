44c8242e541571f88c66863a6cdb0add37a16c10
Lachlan Pham <44896700+lache-moi@users.noreply.github.com>
2022-04-08 16:08:22 +0400

    basic UI

--- source/background.py
+++ source/background.py
@@ -1,6 +1,12 @@
 from background_loan import Background_Loan
 from background_stock import Background_Stock
-from timeline import Timeline
+
+# from timeline import Timeline
+
+import pygame
+import pygame_gui
+from pygame_gui import UIManager, PackageResource
+from pygame_gui.elements import UIButton, UILabel, UIPanel, UITextBox
 
 
 class Background:
@@ -10,23 +16,27 @@ class Background:
     # find a way to create a private constructor
     def __init__(
         self,
-        timelines: list(Timeline),
-        stocks: list(Background_Stock),
-        loan: Background_Loan,
-        transaction_cost: int,
-        win_cond_type: str,
-        win_conds: dict(str, str),
+        # timelines: list(Timeline),
+        # stocks: list(Background_Stock),
+        # loan: Background_Loan,
+        # transaction_cost: int,
+        # win_cond_type: str,
+        # win_conds: dict(str, str),
+        manager: UIManager,
     ) -> None:
-        self.timelines = timelines
-        self.stocks = stocks
-        self.loan = loan
-        self.transation_cost = transaction_cost
-        self.win_cond_type = win_cond_type
-        self.win_conds = win_conds
+        self.creation_button = UIButton(text= "Split Timeline", tool_tip_text = 'Copy the current timeline into two',
+                            relative_rect=pygame.Rect(300,500,200,50),
+                            manager = manager)
+        # self.timelines = timelines
+        # self.stocks = stocks
+        # self.loan = loan
+        # self.transation_cost = transaction_cost
+        # self.win_cond_type = win_cond_type
+        # self.win_conds = win_conds
 
-    def get_instance(self) -> self.Background:
-        # https://www.geeksforgeeks.org/singleton-pattern-in-python-a-complete-guide/
-        pass
+    # def get_instance(self) -> self.Background:
+    #     # https://www.geeksforgeeks.org/singleton-pattern-in-python-a-complete-guide/
+    #     pass
 
     def load_data(self, scenario_info: str) -> None:
         pass
@@ -40,8 +50,8 @@ class Background:
     def split_timelines(self) -> None:
         pass
 
-    def merge_timeline(self, Timeline) -> None:
-        pass
+    # def merge_timeline(self, Timeline) -> None:
+    #     pass
 
     def progress_time(self) -> None:
         pass
--- source/main.py
+++ source/main.py
@@ -3,10 +3,10 @@ import numpy as np
 import pygame
 import pygame_gui
 from pygame_gui import UIManager, PackageResource
-from pygame_gui.elements import UIButton
-from pygame_gui.elements import UILabel
-from pygame_gui.elements import UIPanel
-from pygame_gui.elements import UITextBox
+from pygame_gui.elements import UIButton, UILabel, UIPanel, UITextBox
+
+from background import Background
+
 
 is_running = False
 
@@ -14,19 +14,16 @@ pygame.init()
 
 pygame.display.set_caption('[Game Title]')
 
-screen_length = 1920
-screen_width = 1080
+screen_length = 1366
+screen_width = 768
 window_surface = pygame.display.set_mode((screen_length, screen_width))
 
 
 bg = pygame.Surface((screen_length, screen_width))
 bg.fill(pygame.Color('#A5AAAF'))
+default_manager = UIManager((screen_length, screen_width)) #, 'data/themes/quick_theme.json')
 
-manager = UIManager((screen_length, screen_width)) #, 'data/themes/quick_theme.json')
-
-creation_button = UIButton(text= "Split Timeline", tool_tip_text = 'Copy the current timeline into two',
-							relative_rect=pygame.Rect(300,500,200,50),
-							manager = manager)
+background = Background(default_manager)
 
 clock = pygame.time.Clock()
 is_running = True
@@ -37,15 +34,15 @@ while is_running:
 		if event.type == pygame.QUIT:
 			is_running = False
 
-#		if event.type == pygame_gui.UI_BUTTON_PRESSED:
-			#if event.ui_element == button_name:
-             #   passpass
+		if event.type == pygame_gui.UI_BUTTON_PRESSED:
+			if event.ui_element == background.creation_button:
+			  print("i'm here")
 
-		manager.process_events(event)
+		default_manager.process_events(event)
 
-	manager.update(time_delta)
+	default_manager.update(time_delta)
 
 	window_surface.blit(bg, (0, 0))
-	manager.draw_ui(window_surface)
+	default_manager.draw_ui(window_surface)
 
 	pygame.display.update()
