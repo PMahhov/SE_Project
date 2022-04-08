9f7f9df8ccb831f8686480e1fa52622f62314fa4
Mathilde Simoni <57211517+mathildesimoni@users.noreply.github.com>
2022-04-08 16:45:23 +0400

    init_class method

--- source/background.py
+++ source/background.py
@@ -13,42 +13,25 @@ class Background:
 
     _instance = None
 
+    # Called internally by Python when creating an object of a class
     def __new__(cls):
-        if cls._instance is None:
+        if cls._instance is None: # check whether the instance of the variable is None
             print('Creating the object')
-            cls._instance = super(Background, cls).__new__(cls)
-        return cls._instance
+            cls._instance = super(Background, cls).__new__(cls) # create a new object with the super() method
+        return cls._instance # return the instance variable that contains the object of the Background class
 
+    # Initialize attributes of the class
     def init_class(self, manager: UIManager):
         self.creation_button = UIButton(text= "Split Timeline", tool_tip_text = 'Copy the current timeline into two',
                             relative_rect=pygame.Rect(300,500,200,50),
                             manager = manager)
 
-
-    # find a way to create a private constructor
-    # def __init__(
-    #     self,
-    #     # timelines: list(Timeline),
-    #     # stocks: list(Background_Stock),
-    #     # loan: Background_Loan,
-    #     # transaction_cost: int,
-    #     # win_cond_type: str,
-    #     # win_conds: dict(str, str),
-    #     manager: UIManager,
-    # ) -> None:
-    #     self.creation_button = UIButton(text= "Split Timeline", tool_tip_text = 'Copy the current timeline into two',
-    #                         relative_rect=pygame.Rect(300,500,200,50),
-    #                         manager = manager)
-    #     # self.timelines = timelines
-    #     # self.stocks = stocks
-    #     # self.loan = loan
-    #     # self.transation_cost = transaction_cost
-    #     # self.win_cond_type = win_cond_type
-    #     # self.win_conds = win_conds
-
-    # # def get_instance(self) -> self.Background:
-    # #     # https://www.geeksforgeeks.org/singleton-pattern-in-python-a-complete-guide/
-    # #     pass
+    # self.timelines = timelines
+    # self.stocks = stocks
+    # self.loan = loan
+    # self.transation_cost = transaction_cost
+    # self.win_cond_type = win_cond_type
+    # self.win_conds = win_conds
 
     def load_data(self, scenario_info: str) -> None:
         pass
