fed214a6c138a4dbc998cd9350a2217d1718d68a
Lachlan Pham <44896700+lache-moi@users.noreply.github.com>
2022-05-08 23:31:09 +0400

    removed logging

--- source/main.py
+++ source/main.py
@@ -5,10 +5,11 @@ from background import Background
 from pygame_gui import UIManager
 import numpy as np
 from menu import Menu
-import logging
-import os
-from datetime import datetime
-import platform
+
+# import logging
+# import os
+# from datetime import datetime
+# import platform
 
 
 
@@ -18,11 +19,11 @@ screen_height = config["screen_height"]
 screen_width = config["screen_width"]
 
 # set up logging
-if not os.path.exists("logging"):
-    os.makedirs("logging")
-now = datetime.now().strftime("%Y%m%d@%H:%M:%S")
-printable_now = datetime.now().strftime("%Y%m%d at %H:%M:%S")
-logging.basicConfig(filename=os.path.join('logging', f'debug[{now}].log'), encoding='utf-8', level=logging.INFO,format='%(message)s')
+# if not os.path.exists("logging"):
+#     os.makedirs("logging")
+# now = datetime.now().strftime("%Y%m%d@%H:%M:%S")
+# printable_now = datetime.now().strftime("%Y%m%d at %H:%M:%S")
+# logging.basicConfig(filename=os.path.join('logging', f'debug[{now}].log'), encoding='utf-8', level=logging.INFO,format='%(message)s')
 
 
 
